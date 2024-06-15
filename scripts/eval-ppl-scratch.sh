e206ed60f40f2e261c3da97ea3334222e199f837
Pang Jianhui <pjh_520@live.com>
2024-06-15 14:50:59 +0800

    Update eval-ppl-scratch.sh

--- scripts/eval-ppl-scratch.sh
+++ scripts/eval-ppl-scratch.sh
@@ -36,18 +36,18 @@ done
 
 }
 
-datapath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/datasets/wikitext-103/testforppl.bin
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103
+datapath=/jianhuipang_qy3/datasets/wikitext-103/testforppl.bin
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103
 # ppl $datapath $modelpath llama2-7b-cptonred-testsample
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103/checkpoint-500
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-120
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-180
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-240
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-5640
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-saveeachepoch-2
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-saveeachepoch-2/checkpoint-664
-modelpathprefix=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-saveeachepoch-2/checkpoint
-modelpathprefix=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/models/wikiart/punc/allm-juhao-txllarge-fullatt-ctwithanchor-ct664/checkpoint
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103/checkpoint-500
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-120
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-180
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-240
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-5epoch/checkpoint-5640
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-saveeachepoch-2
+modelpath=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-saveeachepoch-2/checkpoint-664
+modelpathprefix=/jianhuipang_qy3/models/checkpoints_ct/punc/allm-juhao-txllarge-wiki103-bigbatch-saveeachepoch-2/checkpoint
+modelpathprefix=/jianhuipang_qy3/models/wikiart/punc/allm-juhao-txllarge-fullatt-ctwithanchor-ct664/checkpoint
 # datapath=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/test/pgforppl/test.bin
 # ppl $datapath $1 $2 $3 $4 $5
 
