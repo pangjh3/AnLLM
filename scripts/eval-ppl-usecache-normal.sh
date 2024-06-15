8990bdc462cce89b5e3dbdc6ae661031c698f89f
Pang Jianhui <pjh_520@live.com>
2024-06-15 14:52:17 +0800

    Update eval-ppl-usecache-normal.sh

--- scripts/eval-ppl-usecache-normal.sh
+++ scripts/eval-ppl-usecache-normal.sh
@@ -34,9 +34,9 @@ python3 eval_ppl_usecache.py \
 
 }
 
-datapath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/gogollm/test/pgforppl/test_sampled_data.bin
-modelpath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/opensourcellms/Llama-2-7b-hf
-# modelpath=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/checkpoints_ct/punc/allm-juhao-7b
+datapath=/jianhuipang_qy3/gogollm/test/pgforppl/test_sampled_data.bin
+modelpath=/jianhuipang_qy3/opensourcellms/Llama-2-7b-hf
+# modelpath=/jianhuipang/gogollm/newmodels/checkpoints_ct/punc/allm-juhao-7b
 
 for seql in 512 1024 2048 4096;do
     ppl $datapath $modelpath llama2fortxl 8 1  $seql 128
