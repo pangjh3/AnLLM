76565eeea5c8ddaa47a18d399ab3202d89d79285
Pang Jianhui <pjh_520@live.com>
2024-06-15 15:01:52 +0800

    Update run-harness-eval.sh

--- scripts/run-harness-eval.sh
+++ scripts/run-harness-eval.sh
@@ -1,17 +1,16 @@
 
-export http_proxy="http://star-proxy.oa.com:3128" 
-export https_proxy="http://star-proxy.oa.com:3128"
-export HF_HOME=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
-export TRANSFORMERS_CACHE=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
 
-codepath=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/newmodel_harness.py
+export HF_HOME=/jianhuipang_qy3/old_hf_cache
+export TRANSFORMERS_CACHE=/jianhuipang_qy3/old_hf_cache
+
+codepath=/jianhuipang/gogollm/newmodels/newmodel_harness.py
 
 results_path=./test/harness/newestresults
 
 mkdir -p $results_path
 
 prefix=allm-7b-ac-5shot-noanchor
-model_path=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/checkpoints_ct/ac/allm-ac-7b
+model_path=/jianhuipang/gogollm/newmodels/checkpoints_ct/ac/allm-ac-7b
 model_name=boolq2.${prefix}
 python3 $codepath \
     --model hf-causal-experimental \
@@ -26,12 +25,11 @@ python3 $codepath \
 
 
 
-export http_proxy="http://star-proxy.oa.com:3128" 
-export https_proxy="http://star-proxy.oa.com:3128"
-export HF_HOME=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
-export TRANSFORMERS_CACHE=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
 
-codepath=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/newmodel_harness.py
+export HF_HOME=/jianhuipang_qy3/old_hf_cache
+export TRANSFORMERS_CACHE=/jianhuipang_qy3/old_hf_cache
+
+codepath=/jianhuipang/gogollm/newmodels/newmodel_harness.py
 
 results_path=./test/harness/newestresults
 
@@ -39,7 +37,7 @@ mkdir -p $results_path
 
 export CUDA_VISIBLE_DEVICES=6
 prefix=allm-7b-punc-5shot-noanchor
-model_path=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/checkpoints_ct/punc/allm-juhao-7b
+model_path=/jianhuipang/gogollm/newmodels/checkpoints_ct/punc/allm-juhao-7b
 model_name=boolq2.${prefix}
 python3 $codepath \
     --model hf-causal-experimental \
@@ -56,19 +54,18 @@ python3 $codepath \
 # hendrycksTest-* hellaswag,sciq,winogrande,arc_easy,arc_challenge,boolq,piqa,openbookqa,coqa
 
 
-export http_proxy="http://star-proxy.oa.com:3128" 
-export https_proxy="http://star-proxy.oa.com:3128"
-export HF_HOME=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
-export TRANSFORMERS_CACHE=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
 
-codepath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/gogollm/newmodel_harness.py
+export HF_HOME=/jianhuipang_qy3/old_hf_cache
+export TRANSFORMERS_CACHE=/jianhuipang_qy3/old_hf_cache
+
+codepath=/jianhuipang_qy3/gogollm/newmodel_harness.py
 
 results_path=./test/harness/addresults_n_split
 
 mkdir -p $results_path
 
 prefix=allm-7b-punc-5shot-anchor
-model_path=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/opensourcellms/anllm-ac-ct/allm-ac-7b
+model_path=/jianhuipang_qy3/opensourcellms/anllm-ac-ct/allm-ac-7b
 model_name=boolq2.${prefix}
 python3 $codepath \
     --model hf-causal-experimental \
@@ -89,9 +86,9 @@ python3 $codepath \
 
 # export http_proxy="http://star-proxy.oa.com:3128" 
 # export https_proxy="http://star-proxy.oa.com:3128"
-# export HF_HOME=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
-# export TRANSFORMERS_CACHE=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
-# codepath=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/newmodel_harness.py
+# export HF_HOME=/jianhuipang_qy3/old_hf_cache
+# export TRANSFORMERS_CACHE=/jianhuipang_qy3/old_hf_cache
+# codepath=/jianhuipang/gogollm/newmodels/newmodel_harness.py
 
 # results_path=./test/harness/newestresults
 
@@ -99,7 +96,7 @@ python3 $codepath \
 
 # task=boolq
 # prefix=allm-7b-ac-5shot-oneanchorforeach
-# model_path=/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/newmodels/checkpoints_ct/ac/allm-ac-7b
+# model_path=/jianhuipang/gogollm/newmodels/checkpoints_ct/ac/allm-ac-7b
 # model_name=boolq.${prefix}
 # python3 $codepath \
 #     --model hf-causal-experimental \
