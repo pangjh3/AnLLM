a8f714202f520324374e9a177d3f46570c76de9d
Pang Jianhui <pjh_520@live.com>
2024-06-15 15:07:58 +0800

    Update run-harness-neweval-all.sh

--- scripts/run-harness-neweval-all.sh
+++ scripts/run-harness-neweval-all.sh
@@ -2,20 +2,20 @@ export CUDA_VISIBLE_DEVICES=7
 
 export http_proxy="http://star-proxy.oa.com:3128" 
 export https_proxy="http://star-proxy.oa.com:3128"
-export HF_HOME=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
-export TRANSFORMERS_CACHE=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/old_hf_cache
+export HF_HOME=/jianhuipang_qy3/old_hf_cache
+export TRANSFORMERS_CACHE=/jianhuipang_qy3/old_hf_cache
 
-codepath=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/gogollm/newmodel_harness.py
+codepath=/jianhuipang_qy3/gogollm/newmodel_harness.py
 
 results_path=./test/harness/addresults_sent_split
 
-pythonp=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/anaconda3/envs/pyforoldharnessenv/bin/python
+pythonp=/jianhuipang_qy3/anaconda3/envs/pyforoldharnessenv/bin/python
 
 mkdir -p $results_path
 
 
 prefix=allm-7b-ac-5shot-sent
-model_path=/apdcephfs_qy3/share_733425/vinnylywang/jianhuipang_qy3/opensourcellms/anllm-ac-ct/allm-ac-7b
+model_path=/jianhuipang_qy3/opensourcellms/anllm-ac-ct/allm-ac-7b
 model_name=all.${prefix}
 $pythonp $codepath \
     --model hf-causal-experimental \
