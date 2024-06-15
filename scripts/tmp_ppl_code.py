317aefdbb700978595c69fd576dc814d9006890a
Pang Jianhui <pjh_520@live.com>
2024-06-15 14:56:24 +0800

    Update tmp_ppl_code.py

--- scripts/tmp_ppl_code.py
+++ scripts/tmp_ppl_code.py
@@ -23,11 +23,10 @@ from tqdm import tqdm
 import transformers
 # from peft import PeftModel
 # from llama_attn_replace import replace_llama_attn
-# sys.path.append("/apdcephfs/share_733425/vinnylywang/jianhuipang/LLMs4MT/transformers/examples/pytorch/language-modeling")
 # from llama_sft_forward_thisversion import replace_llama_forward, replace_llama_forward_forfastinfer
 
 
-sys.path.append("/apdcephfs/share_733425/vinnylywang/jianhuipang/gogollm/codes")
+sys.path.append("/codes")
 from llama_sft_forward_thisversion_new import replace_llama_forward_forinference_withasan as myinfer
 
 
